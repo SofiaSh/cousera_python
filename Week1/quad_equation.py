35cc5e21e3dcf887d8697e1568af3f7088f41df3
Sofya Shvalova <79528091357@yandex.ru>
2018-05-30 23:44:36 +0700

    full version task 1/3

---
+++ Week1/quad_equation.py
@@ -0,0 +1,8 @@
+from math import sqrt
+a = float(input())
+b = float(input())
+c = float(input())
+x1 = int(-(b+sqrt(b**2-(4*a*c)))/(2*a))
+x2 = int(-(b-sqrt(b**2-(4*a*c)))/(2*a))
+print(x1)
+print(x2)
