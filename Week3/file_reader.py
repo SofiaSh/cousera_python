bfccd1ac0083c6ef89c9c077e8def2766385594d
Sofya Shvalova <79528091357@yandex.ru>
2018-06-12 17:43:17 +0700

    task 3/1

---
+++ Week3/file_reader.py
@@ -0,0 +1,11 @@
+class FileReader:
+    def __init__(self, path):
+        self._path = path
+
+    def read(self):
+        try:
+            with open(self._path)  as f:
+                return f.read()
+        except IOError:
+            return ""
+
